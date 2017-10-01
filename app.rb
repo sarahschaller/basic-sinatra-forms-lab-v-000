b1cb7323648b92b4fb0fdf98fd4d5bccaf7f9b60
Sarah Schaller <sarcsch@aol.com>
2017-10-01 17:41:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post '/team' do 
+  post '/team' do
     @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
@@ -14,7 +14,7 @@ class App < Sinatra::Base
     @pf = params[:pf]
     @c = params[:c]
     
-    erb :team 
+    erb :team
   end
 
 
